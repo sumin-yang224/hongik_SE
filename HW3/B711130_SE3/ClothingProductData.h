b8af53da395da05b7cf3eb70ff056ce3beda2652
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-06-01 23:56:17 +0900

    Update ClothingProductData.h

--- HW3/B711130_SE3/ClothingProductData.h
+++ HW3/B711130_SE3/ClothingProductData.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
-
+#define MAX 50
 using namespace std;
 
 class ClothingProductData
@@ -10,18 +10,18 @@ private:
     string productName;
     string productCompanyName;
     string sellerID;
-    string buyerID;
+    string buyerID[MAX];
     int price;
     int remainQuantity;
     int salesQuantity;
     int average;
-    int satisfaction;
+    int satisfaction[MAX];
     int recentSearch;
 
 public:
     ClothingProductData(string, string, int, int, string);
     string getSellerID();
-    string getBuyerID();
+    string getBuyerID(int);
     string getProductName();
     void setBuyerID(string);
     int getRecentSearch();
@@ -29,9 +29,9 @@ public:
     void downremainQuantity();
     void upsalesQuantity();
     int getRemainQuantity();
+    int getSalesQuantity();
     void setSatisfaction(int);
     void getSalesClothingProductDetails(string*, string*, int*, int*, string*, int*);
-    void createClothingProduct();
     void getSoldOutClothingProductDetails(string*, string*, int*, int*, int*);
     void getClothingProductStatistics(string*, int*, int*);
     
