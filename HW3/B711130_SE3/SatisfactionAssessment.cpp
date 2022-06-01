b9f33fbf8c2a31c5a78f6dab87a69e0692408287
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-06-01 21:26:58 +0900

    Update SatisfactionAssessment.cpp

--- HW3/B711130_SE3/SatisfactionAssessment.cpp
+++ HW3/B711130_SE3/SatisfactionAssessment.cpp
@@ -1,5 +1,17 @@
+#include "SatisfactionAssessment.h"
+#include "UserList.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+SatisfactionAssessment::SatisfactionAssessment(ClothingProductList* pClothingProductList, UserList* userList)
+{
+	this->pClothingProductList = pClothingProductList;
+	pSatisfactionAssessmentUI = new SatisfactionAssessmentUI(this, userList);
+	pSatisfactionAssessmentUI->enterSatisfaction();
+}
 
-SatisfactionAssessment::SatisfactionAssessment() {
-	
+ClothingProductData* SatisfactionAssessment::addSatisfactionOfProduct(string userID, string ProductName, int Satisfaction)
+{
+	return pClothingProductList->addSatisfactionOfProduct(userID, ProductName, Satisfaction);
 }
