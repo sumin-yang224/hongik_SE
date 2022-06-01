bf3ddeb8346b969906a6925e162b979bbb342fc5
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-06-01 21:28:42 +0900

    Update SatisfactionAssessmentUI.h

--- HW3/B711130_SE3/SatisfactionAssessmentUI.h
+++ HW3/B711130_SE3/SatisfactionAssessmentUI.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "UserList.h"
+
+class SatisfactionAssessment;
+
+class SatisfactionAssessmentUI
+{
+private:
+	SatisfactionAssessment* pSatisfactionAssessment;
+	UserList* pUserList;
+public:
+	SatisfactionAssessmentUI(SatisfactionAssessment* satisfactionAssessment, UserList* userList);
+	void enterSatisfaction();
+};
