7c483fbdf150c6c56a6843660fe5fc40e639255f
eseonghyeok <101812352+eseonghyeok@users.noreply.github.com>
2022-06-01 21:28:17 +0900

    Update SatisfactionAssessment.h

--- HW3/B711130_SE3/SatisfactionAssessment.h
+++ HW3/B711130_SE3/SatisfactionAssessment.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "SatisfactionAssessmentUI.h"
+#include "ClothingProductList.h"
+
+
+class SatisfactionAssessment
+{
+private:
+	SatisfactionAssessmentUI* pSatisfactionAssessmentUI;
+	ClothingProductList* pClothingProductList;
+public:
+	SatisfactionAssessment(ClothingProductList* pClothingProductList, UserList* userList);
+	ClothingProductData* addSatisfactionOfProduct(string, string, int);
+};
+
